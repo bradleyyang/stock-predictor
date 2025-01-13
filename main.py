5c74230b68f11e34bc3c088f5230829a3d443fa7
bradleyyang <bradley.yang023@gmail.com>
2025-01-13 16:26:50 -0500

    added exception handling to server

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"stockpredictor": "hello"}
-
 @app.get("/stockprices/{stock}")
 def read_stockprices(stock: str):
     try:
         stock_data = get_stock(stock, "1mo", "1d")
+        if stock_data is None:
+            return {"error": "stock data is none"}
         if stock_data.empty:
             return {"error": "No data found for stock"}
         predictions = get_predictions(10, "B", stock_data)
+        if predictions is None:
+            return {"error": "predictions is none"}
         return {"stock": stock, "predictions": predictions}
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
--- utils.py
+++ utils.py
@@ -3,61 +3,34 @@ from prophet import Prophet
 import yfinance as yf
 import numpy as np
 from prophet.plot import plot_plotly, plot_components_plotly
-import json
 
 def get_stock(ticker: str, period: str, interval: str):
-    data = yf.Ticker(ticker).history(period=period, interval=interval, actions=False)
-    data.drop(axis=1, columns=['Volume', 'Open', 'High', 'Low'], inplace=True)
-    data.rename(columns={'Close': 'y'}, inplace=True)
-    data.reset_index(inplace=True)
-    data.rename(columns={'Date': 'ds'}, inplace=True)
-    data['ds'] = data['ds'].dt.tz_localize(None)
-    data.dropna(inplace=True)
-    return data
+    try:
+        data = yf.Ticker(ticker).history(
+            period=period, interval=interval, actions=False)
+        data.drop(axis=1, columns=['Volume', 'Open',
+                  'High', 'Low'], inplace=True)
+        data.rename(columns={'Close': 'y'}, inplace=True)
+        data.reset_index(inplace=True)
+        data.rename(columns={'Date': 'ds'}, inplace=True)
+        data['ds'] = data['ds'].dt.tz_localize(None)
+        data.dropna(inplace=True)
+        return data
+    except:
+        return None
 
 def get_predictions(periods: int, freq: str, data: pd.DataFrame):
-    model = Prophet()
-    model.fit(data)
+    try:
+        model = Prophet()
+        model.fit(data)
 
-    future = model.make_future_dataframe(periods=periods, freq=freq)
+        future = model.make_future_dataframe(periods=periods, freq=freq)
 
-    forecast = model.predict(future)
+        forecast = model.predict(future)
 
-    predicted_data = forecast[['ds', 'yhat']].copy()
-    predicted_data["ds"] = predicted_data["ds"].dt.strftime('%Y-%m-%d')
-    predicted_json = predicted_data.to_dict(orient="records")
-    return predicted_json
-
-def get_stockprices(ticker: str, period: str, interval: str, future_periods: int, freq: str):
-    data = yf.Ticker(ticker).history(period=period, interval=interval, actions=False)
-    data.drop(axis=1, columns=['Volume', 'Open', 'High', 'Low'], inplace=True)
-    data.rename(columns={'Close': 'y'}, inplace=True)
-    data.reset_index(inplace=True)
-    data.rename(columns={'Date': 'ds'}, inplace=True)
-    data['ds'] = data['ds'].dt.tz_localize(None)
-    data.dropna(inplace=True)
-
-    m = Prophet()
-    m.fit(data)
-
-    future = m.make_future_dataframe(periods=future_periods, freq=freq)
-
-    forecast = m.predict(future)
-
-    predicted_data = forecast[['ds', 'yhat']].copy()
-    predicted_data["ds"] = predicted_data["ds"].dt.strftime('%Y-%m-%d')
-    predicted_json = predicted_data.to_dict(orient="records")
-    return json.dumps(predicted_json, indent=2)
-
-    # fig1 = m.plot(forecast)
-    # fig2 = m.plot_components(forecast)
-
-    # fig1 = plot_plotly(m, forecast)
-    # fig2 = plot_components_plotly(m, forecast)
-    # fig1.show()
-
-stock_data = get_stock("AAPL", "1mo", "1d")
-print(get_predictions(10, "B", stock_data))
-
-
-
+        predicted_data = forecast[['ds', 'yhat']].copy()
+        predicted_data["ds"] = predicted_data["ds"].dt.strftime('%Y-%m-%d')
+        predicted_json = predicted_data.to_dict(orient="records")
+        return predicted_json
+    except:
+        return None
